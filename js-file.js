b6c43c23217201e8f263c64fa19d26dcf91308be
LockpickedKey <zyroisbored@gmail.com>
2024-04-12 17:18:07 +0700

    Made the general outline of the calculator

---
+++ js-file.js
@@ -0,0 +1,17 @@
+function addition(num1, num2){
+      return num1 + num2;
+}
+
+function subtraction(num1, num2){
+      return num1 - num2;
+}
+
+function multiplication(num1, num2){
+      return num1 * num2;
+}
+
+function division(num1, num2){
+      return num1 / num2;
+}
+
+let num1, num2, operator;
