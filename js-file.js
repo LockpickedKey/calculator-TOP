19b415933c9732721bcacd2b187d1bb98f6b6fc2
LockpickedKey <zyroisbored@gmail.com>
2024-04-14 13:09:47 +0700

    Added the ability to update the display through clicking the numbers.

--- js-file.js
+++ js-file.js
@@ -1,6 +1,9 @@
-let value1, value2, result, displayValue;
+let value1, value2, result;
 let operator = '';
+let displayValue = '0';
 
+const buttons = document.querySelectorAll(".number");
+const display = document.querySelector("#display");
 const operate = (n1, op, n2) => {
       switch(op){
           case '+':
@@ -20,3 +23,11 @@ const operate = (n1, op, n2) => {
               break
       }
 };
+
+function updateDisplay(text) {
+      display.innerText = text;
+}
+
+buttons.forEach(button => button.addEventListener("click", () => {
+      updateDisplay(button.value);
+}))
